50fb78bd60d0edfd8c7fa0f120515cdcbfd6c42b
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-04-18 08:26:44 +0900

    物件追加機能実装

--- app/models/informaition.rb
+++ app/models/informaition.rb
@@ -1,4 +1,9 @@
 class Informaition < ApplicationRecord
   has_many :stations
-  accepts_nested_attributes_for :stations
+  accepts_nested_attributes_for :stations, reject_if: :all_blank
+
+  validates :name, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
 end
